7b7d7ce5e9609a5ebdc080007962897bcccfc821
Trigur <trigur@yandex.ru>
2017-05-23 16:37:22 +0300

    Update module_info.php

--- module_info.php
+++ module_info.php
@@ -3,15 +3,15 @@
 (defined('BASEPATH')) OR exit('No direct script access allowed');
 
 $com_info = [
-             'menu_name'   => lang('Image Resize', 'image_resize'), // Menu name
-             'description' => 'Изменение размеров изображений на лету.', // Module Description
-             'admin_type'  => 'window', // Open admin class in new window or not. Possible values window/inside
-             'window_type' => 'xhr', // Load method. Possible values xhr/iframe
-             'w'           => 600, // Window width
-             'h'           => 550, // Window height
-             'version'     => '0.1', // Module version
-             'author'      => 'dev@imagecms.net', // Author info
-             'icon_class'  => 'fa fa-dot-circle-o',// Module menu icon
-            ];
+    'menu_name'   => 'Image Resize',
+    'description' => 'Изменение размеров изображений на лету.',
+    'admin_type'  => 'window',
+    'window_type' => 'xhr',
+    'w'           => 600,
+    'h'           => 550,
+    'version'     => '0.1',
+    'author'      => 'trigur@yandex.ru',
+    'icon_class'  => 'fa fa-picture-o',
+];
 
 /* End of file module_info.php */
